319dcaeacca784a9acfde6aaca78482b644d24af
Vishal Chakradhari <vishalchakradhari2005@gmail.com>
2025-03-03 19:07:14 +0530

    Delete firstprojestjs.js

--- firstprojestjs.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('search-btn').addEventListener('click', () => {
-    const query = document.getElementById('search-bar').value;
-    console.log(`Searching for: ${query}`);
-    // Make an API call to fetch search results
-});
